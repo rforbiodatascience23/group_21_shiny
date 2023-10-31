7fe82b60eda8fc9d52805f57b2777526c43a6bc2
emmaqingjie <s215090@dtu.dk>
2023-10-31 11:18:51 +0100

    module 1 changes

--- R/mod_DNA_to_peptide.R
+++ R/mod_DNA_to_peptide.R
@@ -4,28 +4,32 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 mod_DNA_to_peptide_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+    fluidRow(
+      column(8, "DNA_sequence"),
+      column(4, "random_dna_length", "generate_dna_button")
+    ),
+    "peptide_sequence"
   )
 }
-    
+
 #' DNA_to_peptide Server Functions
 #'
-#' @noRd 
+#' @noRd
 mod_DNA_to_peptide_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+
   })
 }
-    
+
 ## To be copied in the UI
 # mod_DNA_to_peptide_ui("DNA_to_peptide_1")
-    
+
 ## To be copied in the server
 # mod_DNA_to_peptide_server("DNA_to_peptide_1")
