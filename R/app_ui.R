561b5d951b9da75500faf9d2f427525247034dd3
emmaqingjie <s215090@dtu.dk>
2023-10-31 10:22:13 +0100

    Changed panel title

--- R/app_ui.R
+++ R/app_ui.R
@@ -14,7 +14,7 @@ app_ui <- function(request) {
       tabsetPanel(
         tabPanel(title = "DNA to peptide",
                  "DNA_to_peptide"),
-        tabPanel(title = "AA frequency",
+        tabPanel(title = "Amino Acid frequency",
                  mod_AA_frequency_ui("AA_frequency_1"))
       )
     )
